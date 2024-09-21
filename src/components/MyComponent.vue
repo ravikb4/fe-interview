d380b2894eebfa7622881a2116f89d5a6dc483cf
Ravi Kiran P <ravip@knowbe4.com>
2024-09-21 11:02:54 +0530

    Add component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<input type="checkbox" v-model="isChecked" />
+		<label for="checkbox">Checkbox</label>
+
+		<br />
+
+		<input type="radio" id="option1" value="option1" v-model="selectedOption" />
+		<label for="option1">Option 1</label>
+
+		<input type="radio" id="option2" value="option2" v-model="selectedOption" />
+		<label for="option2">Option 2</label>
+
+		<br />
+
+		<input type="text" v-model="textInput" />
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			isChecked: false,
+			selectedOption: '',
+			textInput: ''
+		}
+	}
+}
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
