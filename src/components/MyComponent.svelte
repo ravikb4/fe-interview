dd3e87a54994796cd4afda2effc99ef261c3189b
Ravi Kiran P <ravip@knowbe4.com>
2024-09-21 12:04:20 +0530

    Simulate loading

--- src/components/MyComponent.svelte
+++ src/components/MyComponent.svelte
@@ -1,4 +1,5 @@
 <script>
+	let loading = true
 	let checkboxValue = false
 	let radioValue = ''
 	let inputValue = ''
@@ -14,29 +15,38 @@
 	function handleInputChange(event) {
 		inputValue = event.target.value
 	}
+
+	// Simulate loading
+	setTimeout(() => {
+		loading = false
+	}, 1000)
 </script>
 
-<div>
-	<label>
-		<input type="checkbox" bind:checked={checkboxValue} on:change={handleCheckboxChange} />
-		Checkbox
-	</label>
-</div>
+{#if loading}
+	<p>Loading...</p>
+{:else}
+	<div>
+		<label>
+			<input type="checkbox" bind:checked={checkboxValue} on:change={handleCheckboxChange} />
+			Checkbox
+		</label>
+	</div>
 
-<div>
-	<label>
-		<input type="radio" value="option1" bind:group={radioValue} on:change={handleRadioChange} />
-		Option 1
-	</label>
-	<label>
-		<input type="radio" value="option2" bind:group={radioValue} on:change={handleRadioChange} />
-		Option 2
-	</label>
-</div>
+	<div>
+		<label>
+			<input type="radio" value="option1" bind:group={radioValue} on:change={handleRadioChange} />
+			Option 1
+		</label>
+		<label>
+			<input type="radio" value="option2" bind:group={radioValue} on:change={handleRadioChange} />
+			Option 2
+		</label>
+	</div>
 
-<div>
-	<label>
-		Text Input:
-		<input type="text" bind:value={inputValue} on:input={handleInputChange} />
-	</label>
-</div>
+	<div>
+		<label>
+			Text Input:
+			<input type="text" bind:value={inputValue} on:input={handleInputChange} />
+		</label>
+	</div>
+{/if}
