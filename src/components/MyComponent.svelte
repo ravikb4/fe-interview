e7e34709912216a171e88b8d6318f2afe9a33011
Ravi Kiran P <ravip@knowbe4.com>
2024-09-21 11:00:11 +0530

    Add component

---
+++ src/components/MyComponent.svelte
@@ -0,0 +1,42 @@
+<script>
+	let checkboxValue = false
+	let radioValue = ''
+	let inputValue = ''
+
+	function handleCheckboxChange(event) {
+		checkboxValue = event.target.checked
+	}
+
+	function handleRadioChange(event) {
+		radioValue = event.target.value
+	}
+
+	function handleInputChange(event) {
+		inputValue = event.target.value
+	}
+</script>
+
+<div>
+	<label>
+		<input type="checkbox" bind:checked={checkboxValue} on:change={handleCheckboxChange} />
+		Checkbox
+	</label>
+</div>
+
+<div>
+	<label>
+		<input type="radio" value="option1" bind:group={radioValue} on:change={handleRadioChange} />
+		Option 1
+	</label>
+	<label>
+		<input type="radio" value="option2" bind:group={radioValue} on:change={handleRadioChange} />
+		Option 2
+	</label>
+</div>
+
+<div>
+	<label>
+		Text Input:
+		<input type="text" bind:value={inputValue} on:input={handleInputChange} />
+	</label>
+</div>
